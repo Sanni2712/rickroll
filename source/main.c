e0c7c121b3e6c7449a4174da3f945496c3c580ad
Sanni2712 <sannihithreddy27@gmail.com>
2026-04-22 02:26:26 +0530

    modified code to now overwrite frames

--- source/main.c
+++ source/main.c
@@ -3,25 +3,26 @@
 
 #include "rick.h"
 
-#define FRAME_SIZE 2592  // exact size per frame
-#define DELAY 65000       // ~0.45x speed (adjust)
+#define FS 2592  // exact size per frame to print once, adjusted by trial and error
+#define DELAY 65000       // speed ( also adjusted by trial and error)
 
 int main() {
-    setvbuf(stdout, NULL, _IONBF, 0);
 
-    printf("\033[2J");
+    setvbuf(stdout, NULL, _IONBF, 0);
+    
+   // printf("\033[2J");
 
     int offset = 0;
 
     while (1) {
-        if (offset + FRAME_SIZE >= rick_txt_len) {
+        if (offset + FS >= rick_txt_len) {
             offset = 0;
         }
 
-        printf("\033[H");
-        fwrite(&rick_txt[offset], 1, FRAME_SIZE, stdout);
+       // printf("\033[H");
+        fwrite(&rick_txt[offset], 1, FS, stdout);
 
-        offset += FRAME_SIZE;
+        offset += FS;
 
         usleep(DELAY);
     }
